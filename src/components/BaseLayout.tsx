0b928a180f1cd5525e15df9025105cf37811a99d
viccyrod <victor@gebarski.com>
2024-12-07 09:09:08 -0500

    home page scrollbar

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -8,7 +8,7 @@ import Sidebar from './Sidebar'
 interface BaseLayoutProps {
     children: ReactNode;
     requireAuth?: boolean;
-  }
+}
 
 const BaseLayout = ({ children, requireAuth = true }: BaseLayoutProps) => {
     const { isAuthenticated, isLoading } = useKindeBrowserClient();
@@ -30,11 +30,12 @@ const BaseLayout = ({ children, requireAuth = true }: BaseLayoutProps) => {
     return (
         <div className='flex h-screen w-full overflow-hidden'>
             <Sidebar />
-            <div className="flex-1 flex flex-col overflow-hidden">
+            <main className="flex-1 flex flex-col overflow-y-auto scrollbar-pretty">
                 {children}
-            </div>
+            </main>
         </div>
     )
 }
+
 export default BaseLayout
 
